2d8e2c4e1c071c1c6dd0f2d2c2191ac2ff53c566
Angelo Salazar <kevinangelosalazar@gmail.com>
2024-03-23 10:31:20 -0500

    change url back

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const parser = require('cookie-parser');
 const path = require('path');
 
-const API_URL = 'https://store-proyect.onrender.com/api/v1/'
+const API_URL = 'https://fake-store.azurewebsites.net/api/v1/'
 
 app.set('port', process.env.PORT || 4000);
 app.set('view engine', 'ejs');
